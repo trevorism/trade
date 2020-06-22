c6220efd8f68c73006b01ffd1e517dffe78a022e
trevorism <trevorism@gmail.com>
2020-06-21 21:59:20 -0400

    Cucumber tests

--- src/acceptance/groovy/com/trevorism/gcloud/ContentRootDefinitions.groovy
+++ src/acceptance/groovy/com/trevorism/gcloud/ContentRootDefinitions.groovy
@@ -6,3 +6,33 @@ package com.trevorism.gcloud
 
 this.metaClass.mixin(cucumber.api.groovy.Hooks)
 this.metaClass.mixin(cucumber.api.groovy.EN)
+
+def contextRootContent
+def pingContent
+
+Given(/the testing application is alive/) {  ->
+    try{
+        new URL("http://trade.trevorism.com/ping").text
+    }
+    catch (Exception ignored){
+        Thread.sleep(10000)
+        new URL("http://trade.trevorism.com/ping").text
+    }
+}
+
+When(/I navigate to {word}/) { String url ->
+    contextRootContent = new URL(url).text
+}
+
+When(/I navigate to \\/ping on {word}/) { String url ->
+    pingContent = new URL("${url}/ping").text
+}
+
+Then(/the API returns a link to the help page/) {  ->
+    assert contextRootContent
+    assert contextRootContent.contains("/help")
+}
+
+Then(/pong is returned, to indicate the service is alive/) {  ->
+    assert pingContent == "pong"
+}
--- src/acceptance/resources/features/ContextRoot.feature
+++ src/acceptance/resources/features/ContextRoot.feature
@@ -1,8 +1,12 @@
 Feature: Context Root of this API
-  In order to use the API, it must be available
+  In order to use the Testing API, it must be available
 
-#  Scenario: HTTP GET on the ContextRoot
-#    Given the application is alive
-#    When I navigate to "https://myurl"
-#    Then then a link to the help page is displayed
+  Scenario: ContextRoot
+    Given the testing application is alive
+    When I navigate to https://trade.trevorism.com
+    Then the API returns a link to the help page
 
+  Scenario: Ping
+    Given the testing application is alive
+    When I navigate to /ping on https://trade.trevorism.com
+    Then pong is returned, to indicate the service is alive
