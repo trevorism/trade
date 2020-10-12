dd2e70b91873499bec8c005becefcf54a537f4d4
trevorism <trevorism@gmail.com>
2020-10-12 18:21:23 -0400

    Added trading endpoints for a few orders

--- src/main/groovy/com/trevorism/gcloud/webapi/controller/BalanceController.groovy
+++ src/main/groovy/com/trevorism/gcloud/webapi/controller/BalanceController.groovy
@@ -4,6 +4,8 @@ import com.trevorism.kraken.KrakenClient
 import com.trevorism.kraken.impl.DefaultKrakenClient
 import com.trevorism.kraken.model.AssetBalance
 import com.trevorism.kraken.model.Price
+import com.trevorism.secure.Roles
+import com.trevorism.secure.Secure
 import com.trevorism.threshold.FastThresholdClient
 import com.trevorism.threshold.PingingThresholdClient
 import com.trevorism.threshold.ThresholdClient
@@ -29,7 +31,7 @@ class BalanceController {
     Map checkPairsAgainstThresholds(){
         new PingingThresholdClient().ping()
 
-        def pairs = ["XRPUSD", "XBTUSD", "SCXBT", "ADAXBT", "WAVESXBT", "ICXXBT", "BCHXBT", "ALGOXBT"]
+        def pairs = ["XRPUSD", "XBTUSD", "SCXBT", "ADAXBT", "WAVESXBT"]
         def result = [:]
         pairs.each { pairName ->
             Price price = getPrice(pairName)
@@ -42,6 +44,7 @@ class BalanceController {
     @GET
     @Path("check/{pairName}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Secure(Roles.USER)
     Price getPrice(@PathParam("pairName") String pairName){
         Price price = krakenClient.getCurrentPrice(pairName)
         return price
@@ -49,6 +52,7 @@ class BalanceController {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Secure(Roles.USER)
     Set<AssetBalance> getAccountBalance(){
         krakenClient.getAccountBalances()
     }
---
+++ src/test/groovy/com/trevorism/gcloud/webapi/controller/TradeControllerTest.groovy
@@ -0,0 +1,122 @@
+package com.trevorism.gcloud.webapi.controller
+
+import com.trevorism.kraken.KrakenClient
+import com.trevorism.kraken.model.Asset
+import com.trevorism.kraken.model.AssetBalance
+import com.trevorism.kraken.model.AssetPair
+import com.trevorism.kraken.model.Candle
+import com.trevorism.kraken.model.DateRange
+import com.trevorism.kraken.model.Order
+import com.trevorism.kraken.model.Price
+import com.trevorism.kraken.model.trade.CancelOrderResult
+import com.trevorism.kraken.model.trade.LimitTrade
+import com.trevorism.kraken.model.trade.MarketTrade
+import com.trevorism.kraken.model.trade.StopLossTrade
+import com.trevorism.kraken.model.trade.Trade
+import com.trevorism.kraken.model.trade.TradeResult
+import org.junit.Before
+import org.junit.Test
+
+import java.time.Duration
+
+class TradeControllerTest {
+
+    private TradeController tradeController
+
+    @Before
+    void setup(){
+        tradeController = new TradeController()
+        tradeController.krakenClient = new TestKrakenClient()
+    }
+
+    @Test
+    void testListOpenOrders() {
+        def result =  tradeController.listOpenOrders()
+        assert result
+
+    }
+
+    @Test
+    void testQueryClosedOrders() {
+        def result = tradeController.queryForClosedOrders(null)
+        assert result
+    }
+
+    @Test
+    void testCreateLimitOrder() {
+        def result = tradeController.createLimitTradeOrder(new LimitTrade())
+        assert result
+    }
+
+    @Test
+    void testCreateMarketOrder() {
+        def result = tradeController.createMarketTradeOrder(new MarketTrade())
+        assert result
+    }
+
+    @Test
+    void testCreateStopLossOrder() {
+        def result = tradeController.createStopLossTradeOrder(new StopLossTrade())
+        assert result
+    }
+
+    @Test
+    void testRemoveOrder(){
+        def result = tradeController.removeOrder("1234")
+        assert result
+    }
+
+
+    class TestKrakenClient implements KrakenClient{
+
+        @Override
+        Set<AssetBalance> getAccountBalances() {
+            return null
+        }
+
+        @Override
+        List<Order> getClosedOrders(DateRange dateRange) {
+            [new Order(orderId: "testClosedOrderId")]
+        }
+
+        @Override
+        List<Order> getOpenOrders(DateRange dateRange) {
+            [new Order(orderId: "testOpenOrderId")]
+        }
+
+        @Override
+        TradeResult createOrder(Trade trade) {
+            return new TradeResult(orderDescription: "test")
+        }
+
+        @Override
+        CancelOrderResult deleteOrder(String s) {
+            return new CancelOrderResult()
+        }
+
+        @Override
+        long serverTime() {
+            return 0
+        }
+
+        @Override
+        List<Asset> getAssets() {
+            return null
+        }
+
+        @Override
+        List<AssetPair> getAssetPairs() {
+            return null
+        }
+
+        @Override
+        Price getCurrentPrice(String s) {
+            return null
+        }
+
+        @Override
+        List<Candle> getCandles(String s, Duration duration) {
+            return null
+        }
+    }
+}
