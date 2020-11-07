61dd7638a491cbdc721097cec03d83d268219fb6
trevorism <trevorism@gmail.com>
2020-11-07 12:51:57 -0500

    Fix bug in getting account balance

--- src/main/groovy/com/trevorism/gcloud/service/DefaultTradeService.groovy
+++ src/main/groovy/com/trevorism/gcloud/service/DefaultTradeService.groovy
@@ -24,8 +24,13 @@ class DefaultTradeService implements TradeService {
 
         getAccountBalance().each {
             double assetPrice = 1
-            if(it.assetName.toUpperCase() == "USD"){
+            if(it.assetName.toUpperCase() == "USD" && assetName.toUpperCase() == "USD"){
                 total += it.balance
+                return
+            }
+            if(it.assetName.toUpperCase() == "USD" && assetName.toUpperCase() == "XBT"){
+                Price price = getPrice("xbtusd")
+                total += it.balance / price.last
                 return
             }
             assetPrice = getAssetPrice(assetName, it, assetPrice)
